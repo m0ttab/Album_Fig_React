4c77606f43cfdd6ddd634f5e70c575fd9dd16024
Ana Laura <104836958+spadiniAna@users.noreply.github.com>
2022-10-25 15:05:40 -0300

    n sei se compras ta certo

---
+++ laravel/app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class UsuariosController extends Controller {
+
+    public function index(){
+        return view('usuarios.login');
+    }
+
+    public function validar(Request $form){
+        $credenciais = $form->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        
+
+        // testa as credenciais, já checando o hash
+        if(Auth::attempt($credenciais)){
+            $form->session()->regenerate();
+                
+            return redirect()->route('home');
+                
+        }else{
+            return redirect()->back()->with('erro', 'Usuário ou senha incorretos');
+        }
+    }
+    
+    public function cadastrar(){
+        return view('usuarios.cadastro');
+    }
+
+    public function create(Request $form){
+        $usuario = new Usuario();
+
+        $validar = $form->validate([
+            'nome' => 'required',
+            'email' => 'required',
+            'senha' => 'required',
+            'confirmpassword' => 'required'
+        ]);
+
+        if($form->password != $form->confirmpassword){
+            return redirect()->back()->with('erro', 'Senhas não conferem');
+        }
+
+        $usuario->nome = $form->nome;
+        $usuario->email = $form->email;
+        $usuario->senha = Hash::make($form->senha);
+
+        $usuario->save();
+
+        return redirect()->route('home');
+
+    }
+
+    public function sair(Request $request){
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('home');
+    }
+}
