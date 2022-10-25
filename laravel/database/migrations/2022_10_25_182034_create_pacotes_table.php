c00efaa3b487ebbc155425614377ca78a7c3883e
Bianca Motta <bianca.100904@bento.local>
2022-10-25 15:23:35 -0300

    ajustes migrations

--- laravel/database/migrations/2022_10_25_182034_create_pacotes_table.php
+++ laravel/database/migrations/2022_10_25_182034_create_pacotes_table.php
@@ -6,18 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('pacotes', function (Blueprint $table) {
-            $table->increments("id");
-            
+            $table->id();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        //
+        Schema::dropIfExists('pacotes');
     }
 };
--- laravel/database/migrations/2022_10_25_182115_create_compras_table.php
+++ laravel/database/migrations/2022_10_25_182115_create_compras_table.php
@@ -6,21 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('compras', function (Blueprint $table) {
-            $table->increments("id");
-
-            $table->integer("id_pacote");
-            $table->dateTime('data_hora');
-            $table->foreign('id_pacote')->references('id')->on('pacotes')->onDelete('cascade');
+            $table->id();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        //
+        Schema::dropIfExists('compras');
     }
 };
